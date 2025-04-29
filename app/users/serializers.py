3ea65a5a6d3299ec7e8a006e8213f3c450d0cea4
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-04-29 20:26:29 -0300

    Feat: Added Follow Serializers

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Follow
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
@@ -16,3 +17,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+class FollowSerializer(serializers.ModelSerializer):
+    
+    following = serializers.ReadOnlyField(source='following.username')
+    followed = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+
+        model = Follow
+        fields = ['following', 'followed', 'created_at']
