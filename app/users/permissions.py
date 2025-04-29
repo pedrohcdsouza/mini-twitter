b407d68f5e8ce5e21fadf90a156fba040a9e8a3f
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-04-29 20:28:11 -0300

    Feat: Added permission logical to (Only followed users see your Following/Followed users

---
+++ app/users/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+class IsSelfOrFollowed(BasePermission):
+    # Allow access only to the user themselves or if they are followed by the user
+    def has_permission(self, request, view):
+        username = view.kwargs.get('username')
+        user = request.user
+        if not user.is_authenticated:
+            return False
+        if username == user.username:
+            return True
+        return user.followed_by.filter(followed__username=username).exists()
