ce24083403b5aeda71316a1a00e062d99c798ded
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-04-29 20:26:08 -0300

    Feat: Added Follow Model

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Follow(models.Model):
+
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('following', 'followed')
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"{self.following.username} → {self.followed.username}"
