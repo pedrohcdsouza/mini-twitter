20cd07113d17c16e1ecf33b1ada76b50b6acf99f
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-04-30 11:39:59 -0300

    Feat: Added routes to CRUD Post, Like and See Feed

---
+++ app/post/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views.post import PostCreateView, PostUpdateView, PostDeleteView, PostDetailView, PostLikeView, PostFeedView
+
+urlpatterns = [
+    path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/like/<int:pk>/', PostLikeView.as_view(), name='post-like'), 
+    path('feed/', PostFeedView.as_view(), name='post-feed'),
+]
