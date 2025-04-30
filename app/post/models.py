31e69704e60d7f3b6545fbf591a1cbcf8517d801
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-04-30 11:37:14 -0300

    Feat: Added MODEL to POST

--- app/post/models.py
+++ app/post/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
