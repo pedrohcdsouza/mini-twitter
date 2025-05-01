9d491933853b1e0e2fa11aafef8d4f5a8f8ad8c3
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-04-30 23:51:28 -0300

    Feat: Added FeedView into Feed.py

---
+++ app/post/views/feed.py
@@ -0,0 +1,16 @@
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from ..models import Post
+from ..serializers import PostSerializer
+from ..pagination import FeedPagination
+from users.models import Follow
+
+class FeedView(ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = FeedPagination
+
+    def get_queryset(self):
+        user = self.request.user
+        followed_users = Follow.objects.filter(following=user).values_list('followed', flat=True)
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
