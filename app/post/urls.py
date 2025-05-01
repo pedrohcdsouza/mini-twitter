0b506d09f62c8b80aef4e39a99f39394731eb35f
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-04-30 23:49:32 -0300

    Fix: correcting feed view

--- app/post/urls.py
+++ app/post/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views.post import PostCreateView, PostUpdateView, PostDeleteView, PostDetailView, PostLikeView, PostFeedView
+from .views.post import PostCreateView, PostUpdateView, PostDeleteView, PostDetailView, PostLikeView
+from .views.feed import FeedView
 
 urlpatterns = [
     path('post/create/', PostCreateView.as_view(), name='post-create'),
@@ -7,5 +8,5 @@ urlpatterns = [
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/like/<int:pk>/', PostLikeView.as_view(), name='post-like'), 
-    path('feed/', PostFeedView.as_view(), name='post-feed'),
+    path('feed/', FeedView.as_view(), name='post-feed'),
 ]
